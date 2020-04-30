d61af716e1d463ba0fcec508acd7c9d873c197df
datasg <37591994+datasg@users.noreply.github.com>
2020-04-30 16:22:49 -0400

    Update global.R
    Revised Graph

--- global.R
+++ global.R
@@ -1,17 +1,21 @@
 library(shiny)
+library(shinydashboard)
 library(ggplot2)
 library(scales)
 library(dplyr)
+library(DT)
 
-
-violations=read.csv('ncfinal.csv')
+# Read CSV & Sort Violations Descending
+df=read.csv('ncfinal.csv')
 sum_type_bytime  <- function(cut_time,df){
   df %>% filter(time_cut == cut_time) %>%
     group_by(Violation) %>%
-    summarize(Value_Count=n()) %>% 
+    summarize(Value_Count=dplyr::n()) %>% 
     arrange(desc(Value_Count))
-  
+
 }
+
+# Convert Times from AM,PM format To Minutes
 
 clean_time <- function(x){
   PorA = substr(x,6,6) #nchar(x)-n+1, nchar(x))
@@ -30,6 +34,17 @@ clean_time <- function(x){
   return (DataCut)
 }
 
+# Test Sum_By_Type
+sum_type_bytime('0-3',df)
+
+# Time Ranges: 0-3 (12 AM- 3 AM) to 21-24 (9 PM- 12 AM)
 time_ranges= c('0-3','3-6','6-9','9-12','12-15','15-18','18-21','21-24')
 
-sum_type_bytime('0-3',violations)
+
+# graph Of Top 10 Tickets By Time 
+max_violation=df %>% group_by(Violation)%>% summarize(Value_Count=dplyr::n()) %>% arrange(desc(Value_Count))
+max_violation$Violation[1:10]
+top_violations=max_violation$Violation[1:10]
+q<- df %>% filter(Violation %in% top_violations) %>% 
+ggplot() + geom_bar(aes(x=Violation, fill=clean_time(Violation.Time)), position= 'dodge')
+q + theme(axis.text.x = element_text(angle = 90, hjust = 1))
